a2462775b6b8f6961c5bb41f3ec69912a800245d
yirui Dupuy <yirui.hao@gmail.com>
2025-01-25 00:19:37 +0100

    fixed the problem

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;;
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))

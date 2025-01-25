8eabcce8a687e6febed3fb245eeb748619de6ce1
yirui Dupuy <yirui.hao@gmail.com>
2025-01-25 08:49:41 +0100

    fixed

--- models/connection.js
+++ models/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionString = `mongodb+srv://admin:${process.env.CONNECTION_STRING}@cluster0.hjonx.mongodb.net/`;
 
+console.log(connectionString);
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
